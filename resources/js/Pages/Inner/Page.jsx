1be3cabe2f442a1912f56d3d682dd7f0c410fb7e
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-01-05 23:22:43 -0300

    added tailwindcss

--- resources/js/Pages/Inner/Page.jsx
+++ resources/js/Pages/Inner/Page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page2 = () => {
     return (
-        <h1>Page2</h1>
+        <h1 className="text-3xl font-bold underline">Page2</h1>
     )
 }
 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import '../css/app.css'
 
 createInertiaApp({
   resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
